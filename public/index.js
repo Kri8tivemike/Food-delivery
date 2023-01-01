2b604f6703e47e00b3f8d366f217cf27afd6a1ff
Idoma Ngbede <idomakevin@gmail.com>
2023-01-01 21:17:23 +0100

    A simple tailwindcss project

---
+++ public/index.js
@@ -0,0 +1,7 @@
+// Initialization of elements
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+  menu.classList.toggle('show');
+});
